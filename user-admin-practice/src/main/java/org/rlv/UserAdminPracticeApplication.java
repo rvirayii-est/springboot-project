a297bfea20a6f2ce1c3ccdb31ba4b399a4156383
rvirayii-est <robertovirayii@gmail.com>
2025-08-06 11:06:58 +0800

    started new user-role base backend

---
+++ user-admin-practice/src/main/java/org/rlv/UserAdminPracticeApplication.java
@@ -0,0 +1,13 @@
+package org.rlv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAdminPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAdminPracticeApplication.class, args);
+	}
+
+}
---
+++ user-admin-practice/src/test/java/org/rlv/UserAdminPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rlv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAdminPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
